856943252e99e983712b8b21ce54474933931317
amansharma112 <iamansharma112@gmail.com>
2025-03-22 17:25:11 +0530

    do

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -25,13 +25,13 @@ export default function Login() {
                         </div>
                         <div className='flex my-2 w-full justify-between'>
                             <div className='flex'>
-                            <input className='ml-2 border-2 rounded-md' type="checkbox" />
-                            <h5 className='text-md mr-10'>Remember me</h5>
+                                <input className='ml-2 border-2 rounded-md' type="checkbox" />
+                                <h5 className='text-md mr-10'>Remember me</h5>
                             </div>
                             <h5 className='text-left font-bold'>Forgot Password?</h5>
                         </div>
 
-                        <div>
+                        <div className='md:w-[450px]'>
                             <button type="button" className="w-full text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
                         </div>
                     </form>
